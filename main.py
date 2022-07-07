75494ae2bd6cb649b3f7c7812bacee267aac0848
Rishaan Desai <rishaandesai41@gmail.com>
2022-07-07 16:18:26 -0700

    Added ErrorHandlers

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, g, session, abort
 from waitress import serve
-from static import blog
+from static import blog, errors
 
-app = Flask('app')
+app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
 
 #initialize the blueprint
 app.register_blueprint(blog.blog, url_prefix='/blog')
+app.register_blueprint(errors.errors)
 
 app.run(host='0.0.0.0', port='8080', debug=True) 
--- static/errors.py
+++ static/errors.py
@@ -1,45 +1,49 @@
-from distutils.log import error
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, g, session, abort
 
-errors = Blueprint(
-    "errors",
-    __name__,
-    template_folder="./templates",
-    static_folder="./static",
-)
+#no url prefix
+errors = Blueprint('errors', __name__, template_folder='./templates', static_folder='./static')
 
-@errors.errorhandler('404')
-def not_found(error):
-    return '<h1>Error 404: Page not found</h1>', 404
+@errors.app_errorhandler(404)
+def error_404(error):
+    message="Error 404: Page not found"
+    return render_template('error.html', message=message), 404
 
-@errors.errorhandler('500')
-def internal_server_error(error):
-    return '<h1>Error 500: Internal server error</h1>', 500
+@errors.app_errorhandler(500)
+def error_500(error):
+    message="Error 500: Internal server error"
+    return render_template('error.html', message=message), 500
 
-@errors.errorhandler('403')
-def forbidden(error):
-    return '<h1>Error 403: Forbidden</h1>', 403
+@errors.app_errorhandler(403)
+def error_403(error):
+    message="Error 403: Forbidden"
+    return render_template('error.html', message=message), 403
 
-@errors.errorhandler('401')
-def unauthorized(error):
-    return '<h1>Error 401: Unauthorized</h1>', 401
+@errors.app_errorhandler(400)
+def error_400(error):
+    message="Error 400: Bad request"
+    return render_template('error.html', message=message), 400
 
-@errors.errorhandler('400')
-def bad_request(error):
-    return '<h1>Error 400: Bad request</h1>', 400
+@errors.app_errorhandler(401)
+def error_401(error):
+    message="Error 401: Unauthorized"
+    return render_template('error.html', message=message), 401
 
-@errors.errorhandler('408')
-def request_timeout(error):
-    return '<h1>Error 408: Request timeout</h1>', 408
+@errors.app_errorhandler(405)
+def error_405(error):
+    message="Error 405: Method not allowed"
+    return render_template('error.html', message=message), 405
 
-@errors.errorhandler('502')
-def bad_gateway(error):
-    return '<h1>Error 502: Bad gateway</h1>', 502
+@errors.app_errorhandler(502)
+def error_502(error):
+    message="Error 502: Bad gateway"
+    return render_template('error.html', message=message), 502
 
-@errors.errorhandler('503')
-def service_unavailable(error):
-    return '<h1>Error 503: Service unavailable</h1>', 503
+@errors.app_errorhandler(503)
+def error_503(error):
+    message="Error 503: Service unavailable"
+    return render_template('error.html', message=message), 503
 
-@errors.errorhandler('504')
-def gateway_timeout(error):
-    return '<h1>Error 504: Gateway timeout</h1>', 504
+@errors.app_errorhandler(504)
+def error_504(error):
+    message="Error 504: Gateway timeout"
+    return render_template('error.html', message=message), 504
